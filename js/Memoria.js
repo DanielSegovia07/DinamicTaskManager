d57af62e04acfc892c6024083f94daa1dc0ff0c0
DanielSegovia07 <danielsegovia671@gmail.com>
2023-10-30 19:42:54 -0500

    prueba daniel

--- js/Memoria.js
+++ js/Memoria.js
@@ -7,7 +7,7 @@ class Memoria {
     }
 
     Inicializar(t, pr) {
-        // Inicializa la memoria con el espacio total y la parte residenteee.
+        // Inicializa la memoria con el espacio total y la parte residenteeee.
         this._dblEspacioTotal = t;
         this._dblParteResidente = pr;
         this._dblEspacioDisponible = t - pr;
